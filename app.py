136744f697c17925291a0e95f015785d0b8ecd2c
Toluwanimi Sonuga <sonugatoluwanimi@gmail.com>
2024-01-12 12:39:32 +0100

    kl

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -45,28 +43,8 @@ def index():
     cars = Car.query.all()
     return render_template('index.html', cars=cars)
 
-# ... other routes ...
-class UserForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    submit = SubmitField('Add User')
+# # ... other routes ...
 
-# Define the route for adding a user
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
-    form = UserForm()
-
-    if form.validate_on_submit():
-        # Create a new user based on the form data
-        new_user = User(name=form.name.data)
-        
-        # Add the user to the database
-        db.session.add(new_user)
-        db.session.commit()
-
-        # Redirect to the index page or any other desired page
-        return redirect(url_for('index'))
-
-    return render_template('add_user.html', form=form)
 
 
 # Run the Application
